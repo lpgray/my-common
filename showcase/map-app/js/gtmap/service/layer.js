4ed3572fe4ac0559eb4ebfea2aee1f83921a5c27
lpgray <rayzy1991@gmail.com>
2013-08-12 15:13:46 +0800

    layer show ? hide impl.

--- showcase/map-app/js/gtmap/service/layer.js
+++ showcase/map-app/js/gtmap/service/layer.js
@@ -4,7 +4,8 @@ define(function(){
         console.log('对比功能启用');
       }
       , 'layerShowHide' : function(){
-        console.log('图层显示、隐藏功能启用');
+        var $self = $(this);
+        map.getLayer($self.attr('data-id'))[ $self.is(':checked') ? 'show' : 'hide' ]();
       }
     }
   }
