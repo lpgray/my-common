93b4fe76edfed938f9bc668514a7855545239ad9
lpgray <rayzy1991@gmail.com>
2013-09-04 17:18:41 +0800

    add grunt template

---
+++ showcase/grunt-template/GruntFile.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg : grunt.file.readJSON('package.json'),
+    banner : '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + 
+    '<%= grunt.template.today("yyyy-mm-dd") %>\n' + 
+    '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>-<%= pkg.author.email %>; */',
+    // Task configuration.
+    concat : {
+      options : {
+        banner : '<%= banner %>',
+        stripBanners : true
+      },
+      dist : {
+        src : ['src/plugins/*.js'], 
+        dest : 'dist/plugins.js'
+      }
+    },  //
+    uglify : {
+      options : {
+        banner : '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n' // 压缩后的文件声明
+      },
+      build : {
+        src : 'dist/plugins.js',
+        dest : 'dist/plugins.min.js'// 压缩后的文件
+      }
+    },
+    qunit : {
+      files: ['test/**/*.html']
+    },
+    hint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+    },
+    watch : {
+      files: '<config:hint.files>',
+      tasks: 'hint qunit'
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  
+  grunt.registerTask('build', ['concat','uglify']);
+  grunt.registerTask('test', ['qunit']);
+  grunt.registerTask('hint', ['hint']);
+  grunt.registerTask('watch',['watch']);
+};
