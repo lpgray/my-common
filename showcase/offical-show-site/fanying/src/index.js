6696c0e75f0d8cde78149fb228bc4d21e25a48ee
lpgray <rayzy1991@gmail.com>
2014-03-26 23:45:49 +0800

    finished three pages

--- showcase/offical-show-site/fanying/src/index.js
+++ showcase/offical-show-site/fanying/src/index.js
@@ -14,7 +14,9 @@ var lastPage = 0
     , currentPage = 1
     , totalPage = 0
     , pageTransition = true
-    , has3d;
+    , has3d
+    , pageEvents = {}
+    , pageReverts = {};
 function has3d() {
     var e = document.createElement("p")
     , t
@@ -35,11 +37,14 @@ function has3d() {
     document.body.removeChild(e);
     return t !== undefined && t.length > 0 && t !== "none"
 }
-function goToPage(lastPage, targetPage, mode) {
-    var $page = $("#page")
-        , $pages = $(".page")
-        , $targetPage = $("#page-" + targetPage)
-        , $indicators = $("#page-indicator li");
+function goToPage(targetPage, mode) {
+    var $targetPage = $("#page-" + targetPage);
+
+    if(lastPage && lastPage != targetPage){
+        $('#page-' + lastPage).removeClass('page' + lastPage + '-loaded');
+        pageReverts[lastPage] && pageReverts[lastPage].call();
+    }
+
     if (mode == "slide" && lastPage != targetPage) {
         if (has3d) {
             $page.addClass("animate").css({
@@ -76,7 +81,7 @@ function goToPage(lastPage, targetPage, mode) {
             } else {
                 $page.css({top: -winHeight * (targetPage - 1)})
             }
-        }, 1000)
+        }, 1000);
     } else {
         if (has3d) {
             $page.removeClass("animate").css({
@@ -98,13 +103,18 @@ function goToPage(lastPage, targetPage, mode) {
         setTimeout(function() {
             pageTransition = false;
             lastPage = currentPage;
-        }, 1000);
-        // pageEvent[t]()
+            $targetPage.addClass('page'+targetPage+'-loaded');
+            pageEvents[targetPage] && pageEvents[targetPage].call();
+        }, 1200);
     }
 }
 $(function() {
     has3d = has3d();
-    
+
+    $page = $("#page");
+    $pages = $(".page");
+    $indicators = $("#page-indicator li");
+
     if (window.isIE6 || window.isIE7) {
         $("body").addClass("ieLow")
     }
@@ -115,8 +125,8 @@ $(function() {
     $(window).resize(function() {
         resetPage()
     });
-    goToPage(lastPage, currentPage);
-    totalPage = $(".page:visible").length;
+    goToPage(currentPage);
+    totalPage = $(".page").length;
     
     var e = 1;
     $(document).bind("mousewheel", function(t, i, a, n) {
@@ -124,12 +134,12 @@ $(function() {
             if (i <= -e) {
                 if (currentPage + 1 <= totalPage) {
                     currentPage = currentPage + 1;
-                    goToPage(lastPage, currentPage, "slide")
+                    goToPage(currentPage, "slide")
                 }
             } else if (i >= e) {
                 if (currentPage - 1 >= 1) {
                     currentPage = currentPage - 1;
-                    goToPage(lastPage, currentPage, "slide")
+                    goToPage(currentPage, "slide")
                 }
             }
         }
@@ -138,7 +148,7 @@ $(function() {
     $("#page-indicator li, #site-navigation li").click(function() {
         var targetPage = parseInt($(this).find("a").attr("rel"));
         if (!pageTransition)
-            goToPage(lastPage, targetPage, "fade");
+            goToPage(targetPage, "fade");
     });
 
     var $page1 = $('#page-1')
@@ -156,6 +166,98 @@ $(function() {
         }
     }
     $drawerTrig.click(openDrawer);
+
+
+    var $page2 = $('#page-2')
+        , $page2selection = $page2.find('.desc')
+        , $page2layers = $page2.find('.layer')
+        , $pag2layersWrapper = $page2.find('.page2-device');
+    pageEvents[2] = function(){
+        loopWithPause($page2selection, function(idx, item){
+            $(item).addClass('animate');
+            if(idx === $page2selection.length-1){
+                $.each($page2layers, function(idx, layer){
+                    idx++;
+                    $(layer).addClass('layer'+idx+'-active');
+                });
+                setTimeout(function(){
+                    activeTriggerAndLayer(0);
+                }, 1000);
+            }
+        }, 100);
+    }
+    function activeTriggerAndLayer(idx){
+        $page2.find('.desc').eq(idx).addClass('active').siblings().removeClass('active');
+        $.each($page2layers, function(i, item){
+            if(i>=idx){
+                i++;
+                $(item).removeClass('layer' + i + '-ignore');
+            }else{
+                i++;
+                $(item).addClass('layer' + i + '-ignore');
+            }
+        });
+    }
+    $page2selection.mouseover(function(){
+        activeTriggerAndLayer($(this).index());
+    });
+    pageReverts[2] = function(){
+        $page2selection.removeClass('active animate');
+        $page2layers.each(function(idx, item){
+            idx++;
+            $(item).removeClass('layer' + idx + '-ignore layer' + idx + '-active active');
+        });
+    }
+
+
+    var $page3 = $('#page-3')
+        , $timelineLis = $page.find('.timeline li')
+        , timelineLen = $timelineLis.length
+        , $dot = $page3.find('span.dot')
+        , page3Timer = 0
+        , page3AutoRun = 0
+        , page3pause = 0
+        , page3Current = 0;
+    function activeDotAndTimeline(idx) {
+        var $target = $timelineLis.eq(idx);
+        $dot.stop().animate({
+            left: $target.offset().left + $target.width() / 2 - $dot.width() / 2
+        }, 500);
+        $target.addClass("current").siblings().removeClass("current");
+    }
+    $timelineLis.mouseover(function(){
+        var self = this;
+        clearTimeout(page3Timer);
+        page3Timer = setTimeout(function(){
+            page3Current = $(self).index()
+            activeDotAndTimeline(page3Current);
+        }, 500);
+        page3pause = 1;
+    }).mouseout(function(){
+        clearTimeout(page3Timer);
+        page3pause = 0;
+    });
+    pageEvents[3] = function(){
+        pageReverts[3]();
+        activeDotAndTimeline(page3Current);
+        page3Current++;
+        page3AutoRun = setInterval(function(){
+            if(page3pause){
+                return;
+            }
+
+            if(page3Current >= $timelineLis.length){
+                page3Current = 0;
+            }
+
+            activeDotAndTimeline(page3Current);
+            page3Current++;
+        }, 2000);
+    };
+    pageReverts[3] = function(){
+        page3pause = 0;
+        clearInterval(page3AutoRun);
+    }
 });
 function resetPage(e) {
     var e = e || $("div.page");
@@ -167,7 +269,7 @@ function resetPage(e) {
         winHeight = $(window).height();
         e.height(winHeight);
         totalPage = $(".page:visible").length;
-        goToPage(currentPage, currentPage);
+        goToPage(currentPage);
         
         if (winWidth > winHeight) {
             $("body").addClass("horizontal")
@@ -175,4 +277,32 @@ function resetPage(e) {
             $("body").removeClass("horizontal")
         }
     }, 100);
+}
+
+
+/**
+ * 延迟遍历数组
+ * @param  {[type]} arr  [数组]
+ * @param  {[type]} func [回调]
+ * @param  {[type]} time [延迟时间]
+ */
+function loopWithPause(arr, func, time){
+    if(!arr.length){
+        return;
+    }
+
+    function one(idx, arr){
+        if(idx === arr.length){
+            return;
+        }else{
+            setTimeout(function(){
+                func.call(arr[idx], idx, arr[idx]);
+                idx++;
+                one(idx, arr);
+            }, time);
+        }
+    }
+
+    func.call(arr[0], 0, arr[0]);
+    one(1, arr);
 }
